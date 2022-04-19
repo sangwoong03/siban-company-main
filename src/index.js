342065322caf312c3e4cc71449f66df2052e73ad
sangwoong <sangwoong03@gmail.com>
2022-04-19 23:10:41 +0900

    siban index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
-
+import { BrowserRouter } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>,
 );
