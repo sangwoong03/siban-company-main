49b0e518566a6207df20bc2f9fa335068e348110
sangwoong <sangwoong03@gmail.com>
2022-05-10 18:32:05 +0900

    activity Router

--- src/routes/activityRouter.js
+++ src/routes/activityRouter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import ActivityMainSection from "./activitySection";
 
 const ActivityHeader = styled.header`
 	margin: 0 auto 20px auto;
@@ -13,66 +14,17 @@ const ActivityHeader = styled.header`
 	font-weight: bold;
 `;
 const ActivityWrapper = styled.div`
-	max-width: 1200px;
+	width: 1200px;
 	margin: 0 auto 100px auto;
-	height: 600px;
 	display: flex;
 `;
-const SideNav = styled.div`
-	width: 200px;
-	height: 100%;
-	border-right: 1px solid #c2c2c2;
-	text-align: center;
-	& > ul > li {
-		font-size: 1.2rem;
-		margin: 50px 0;
-		cursor: pointer;
-	}
-	& > ul > li:hover {
-		color: rgba(0, 0, 0, 0.4);
-		text-decoration: underline;
-	}
-`;
-const MainSection = styled.section`
-	width: 100%;
-	height: 100%;
-	background: green;
-`;
 
-const activitytInfo = [
-	{
-		title: "",
-		name: "",
-		age: "",
-		where: "",
-		desc: "",
-		projects: "",
-		url: "",
-	},
-	{
-		title: "",
-		name: "",
-		age: "",
-		where: "",
-		desc: "",
-		projects: "",
-		url: "",
-	},
-];
 function ArtistRouter() {
 	return (
 		<>
 			<ActivityHeader>[시: 반] 일 상 </ActivityHeader>
 			<ActivityWrapper>
-				<SideNav>
-					<ul>
-						<li> 공 연 </li>
-						<li> 영 상 </li>
-						<li> 레 슨 </li>
-						<li> 일상모습 </li>
-					</ul>
-				</SideNav>
-				<MainSection></MainSection>
+				<ActivityMainSection />
 			</ActivityWrapper>
 		</>
 	);
