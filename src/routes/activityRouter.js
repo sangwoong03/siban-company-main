f21908f2eab4ca4b5aac1d802c8a75b5bd460cfe
sangwoong <sangwoong03@gmail.com>
2022-05-11 18:01:19 +0900

    activity Router

--- src/routes/activityRouter.js
+++ src/routes/activityRouter.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import ActivityMainSection from "./activitySection";
+import ActivityMainSection from "./activityMainSection";
+import ActivityVideoSection from "./activityVideoSection";
+import ActivityLessonSection from "./activityLessonSection";
+import ActivityDailySection from "./activityDailySection";
 
 const ActivityHeader = styled.header`
 	margin: 0 auto 20px auto;
@@ -13,21 +16,68 @@ const ActivityHeader = styled.header`
 	font-size: 2.2rem;
 	font-weight: bold;
 `;
+
 const ActivityWrapper = styled.div`
 	width: 1200px;
 	margin: 0 auto 100px auto;
 	display: flex;
 `;
 
-function ArtistRouter() {
+const SideNav = styled.div`
+	width: 200px;
+	height: auto;
+	text-align: center;
+	background: rgba(0, 0, 0, 0.05);
+	& > ul > li {
+		font-size: 1.1rem;
+		font-weight: bold;
+		margin: 50px 0;
+		cursor: pointer;
+	}
+	& > ul > li:hover {
+		color: rgba(0, 0, 0, 0.4);
+		text-decoration: underline;
+	}
+`;
+function ActivityRouter() {
+	const [renderSection, setRenderSection] = useState(<ActivityMainSection />);
+	const [subTitle, setSubTitle] = useState("공연");
+	const ActivityNavList = ["공연", "영상", "레슨", "일상"];
+	const sectionList = [
+		<ActivityMainSection />,
+		<ActivityVideoSection />,
+		<ActivityLessonSection />,
+		<ActivityDailySection />,
+	];
+
+	const changeRender = (e) => {
+		const listId = parseInt(e.target.id);
+
+		for (let i = 0; i < ActivityNavList.length; i++) {
+			if (listId === i) {
+				setRenderSection(sectionList[i]);
+				setSubTitle(ActivityNavList[i]);
+			}
+		}
+	};
 	return (
 		<>
-			<ActivityHeader>[시: 반] 일 상 </ActivityHeader>
+			<ActivityHeader>[시: 반] 일 상 - {subTitle} </ActivityHeader>
+
 			<ActivityWrapper>
-				<ActivityMainSection />
+				<SideNav>
+					<ul>
+						{ActivityNavList.map((item, index) => (
+							<li key={index} id={index} onClick={changeRender}>
+								{item}
+							</li>
+						))}
+					</ul>
+				</SideNav>
+				{renderSection}
 			</ActivityWrapper>
 		</>
 	);
 }
 
-export default ArtistRouter;
+export default ActivityRouter;
