aa81c2c698986d038898e60d26ecd62cedad7927
sangwoong <sangwoong03@gmail.com>
2022-05-11 17:13:30 +0900

    update board router

--- src/routes/boardRouter.js
+++ src/routes/boardRouter.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const Board = styled.div`
+	font-size: 1.3rem;
+	font-weight: bold;
+	padding: 100px 0;
+	height: 40vh;
+	text-align: center;
+`;
 
 function BoardRouter() {
-	return <div> 게시판 </div>;
+	return <Board> 준비 중입니다. </Board>;
 }
 
 export default BoardRouter;
