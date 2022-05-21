1830589f4a65dcf5d047a6c267adfa53156196c2
sangwoong <sangwoong03@gmail.com>
2022-05-21 14:16:08 +0900

    side navigation bar component for router pages

---
+++ src/routes/RouterSideNav.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+function SideNavForRouter({ children }) {
+	const SideNav = styled.div`
+		width: 200px;
+		height: auto;
+		text-align: center;
+		background: rgba(0, 0, 0, 0.05);
+		& > ul > li {
+			font-size: 1.1rem;
+			font-weight: bold;
+			margin: 50px 0;
+			cursor: pointer;
+		}
+		& > ul > li:hover {
+			color: rgba(0, 0, 0, 0.4);
+			text-decoration: underline;
+		}
+	`;
+	return <SideNav> {children} </SideNav>;
+}
+
+export default SideNavForRouter;
