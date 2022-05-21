1a85b76836af599f20c8ed4eabb44b3d14e7b70b
sangwoong <sangwoong03@gmail.com>
2022-05-21 14:15:51 +0900

    header component for router pages

---
+++ src/routes/RouterHeader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const RouterHeader = styled.header`
+	margin: 0 auto 20px auto;
+	padding: 100px 0 0 0;
+	border-bottom: 1px solid #e5e5e5;
+	max-width: 860px;
+	height: 70px;
+	display: flex;
+	align-items: center;
+	font-size: 2.2rem;
+	font-weight: bold;
+`;
+
+function HeaderForRouter({ page }) {
+	return <RouterHeader>{page}</RouterHeader>;
+}
+
+export default HeaderForRouter;
