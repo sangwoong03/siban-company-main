dd3d25031651e68319ee3e002e3e0b218785eafd
sangwoong <sangwoong03@gmail.com>
2022-05-21 14:16:46 +0900

    update board router

--- src/routes/boardRouter.js
+++ src/routes/boardRouter.js
@@ -1,16 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import HeaderForRouter from "./RouterHeader";
+import WrapperForRouter from "./RouterWrapper";
 
 const Board = styled.div`
 	font-size: 1.3rem;
 	font-weight: bold;
-	padding: 100px 0;
-	height: 40vh;
+	margin: 200px auto 0 auto;
 	text-align: center;
 `;
 
 function BoardRouter() {
-	return <Board> 준비 중입니다. </Board>;
+	return (
+		<div>
+			<HeaderForRouter page={"[시: 반] 소통"} />
+			<WrapperForRouter>
+				<Board>준비 중입니다.</Board>
+			</WrapperForRouter>
+		</div>
+	);
 }
 
 export default BoardRouter;
