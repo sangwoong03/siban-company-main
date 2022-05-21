0f3f60689cc6ec6d72eb484b3a342a305de299de
sangwoong <sangwoong03@gmail.com>
2022-05-21 14:16:40 +0900

    update artist router

--- src/routes/artistRouter.js
+++ src/routes/artistRouter.js
@@ -1,32 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import ArtistMainSection from "./artists/artistSection";
-
-const ArtistsHeader = styled.header`
-	margin: 0 auto 20px auto;
-	padding: 100px 0 0 0;
-	border-bottom: 1px solid #e5e5e5;
-	max-width: 860px;
-	height: 70px;
-	display: flex;
-	align-items: center;
-	font-size: 2.2rem;
-	font-weight: bold;
-`;
-const ArtistsWrapper = styled.div`
-	max-width: 1200px;
-	margin: 0 auto 100px auto;
-	height: 600px;
-	display: flex;
-`;
+import HeaderForRouter from "./RouterHeader";
+import WrapperForRouter from "./RouterWrapper";
 
 function ArtistRouter() {
 	return (
 		<>
-			<ArtistsHeader>[시: 반] 아티스트</ArtistsHeader>
-			<ArtistsWrapper>
+			<HeaderForRouter page={"[시: 반] 아티스트"} />
+			<WrapperForRouter>
 				<ArtistMainSection />
-			</ArtistsWrapper>
+			</WrapperForRouter>
 		</>
 	);
 }
