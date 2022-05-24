ec94331a332de53d497dab59758093d0ee1fa6aa
sangwoong <sangwoong03@gmail.com>
2022-05-24 23:26:31 +0900

    update board router - layout for notice section

--- src/routes/boardRouter.js
+++ src/routes/boardRouter.js
@@ -7,11 +7,37 @@ import SideNavForRouter from "./RouterSideNav";
 const Board = styled.div`
 	font-size: 1.3rem;
 	font-weight: bold;
-	width: 1200px;
+	width: 100%;
 	height: 600px;
 	text-align: center;
 	& p {
 		padding-top: 200px;
+	}
+	& ul {
+		display: flex;
+		width: 80%;
+		margin: 0 auto 0 30px;
+	}
+	& .notice__list:hover {
+		background: rgba(0, 0, 0, 0.03);
+	}
+	& .notice__title {
+		padding: 20px 0;
+		font-size: 1rem;
+		border-bottom: 1px solid #d5d5d5;
+	}
+	& .notice__list {
+		font-size: 1rem;
+		padding: 20px 0;
+		border-bottom: 1px solid #e7e7e7;
+	}
+	& ul li:nth-child(1),
+	& ul li:nth-child(2),
+	& ul li:nth-child(4) {
+		width: 15%;
+	}
+	& ul li:nth-child(3) {
+		width: 55%;
 	}
 `;
 
@@ -43,7 +69,32 @@ function BoardRouter() {
 					</ul>
 				</SideNavForRouter>
 				<Board>
-					<p>준비 중입니다.</p>
+					<section className="notice--section">
+						<ul className="notice__title">
+							<li> 번호 </li>
+							<li> 날짜 </li>
+							<li> 제목 </li>
+							<li> 글쓴이 </li>
+						</ul>
+						<ul className="notice__list">
+							<li>1</li>
+							<li>2022-05-24</li>
+							<li>첫번째 공지사항입니다.</li>
+							<li> Siban </li>
+						</ul>
+						<ul className="notice__list">
+							<li>2</li>
+							<li>2022-05-24</li>
+							<li>두번째 테스트입니다.</li>
+							<li> Admin </li>
+						</ul>
+						<ul className="notice__list">
+							<li>3</li>
+							<li>2022-05-24</li>
+							<li>세번째 공지사항 테스트입니다.</li>
+							<li> Siban </li>
+						</ul>
+					</section>
 				</Board>
 			</WrapperForRouter>
 		</div>
